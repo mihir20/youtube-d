9278c2693f0152ce17cf443f9240919f7cd50062
mihir20 <mihir20121997@gmail.com>
2020-06-14 16:56:37 +0530

    showing video details

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function createWindow() {
   // receive message from index.html 
   ipcMain.on('asynchronous-message', (event, arg) => {
     console.log(arg);
-    downLoadVideo(arg);
+    downLoadVideo(arg,event);
     // send message to index.html
-    event.sender.send('asynchronous-reply', 'hello');
+    // event.sender.send('asynchronous-reply', 'hello');
   });
 }
 
-function downLoadVideo(vidUrl) {
+function downLoadVideo(vidUrl,event) {
   const url = 'http://www.youtube.com/watch?v=WKsjaOqDXgg';
   youtubedl.getInfo(url, (err,info) => {
     if (err) throw err
@@ -37,6 +37,7 @@ function downLoadVideo(vidUrl) {
     console.log('description:', info.description)
     console.log('filename:', info._filename)
     console.log('format id:', info.format_id)
+    event.sender.send('asynchronous-reply', {info});
     downloadWithFileName(url,info._filename);
   })
 }
--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
 $(document).ready(()=>{
+  $('#footer').hide();
     $('#submitUrlBtn').click(()=>{
         var url = $('#urlInput').val();
         
     const {ipcRenderer} = require('electron')
 
-    // send username to main.js 
+    // send url to index.js 
     ipcRenderer.send('asynchronous-message', url )
 
-    // receive message from main.js
+    // receive message from index.js
     ipcRenderer.on('asynchronous-reply', (event, arg) => {
-      console.log(arg) 
+      console.log(arg);
+      if(arg.info){
+        setInfo(arg.info);
+      } 
 
     })
 
     })
 })
+
+const setInfo = (info)=>{
+  $('#footer').show();
+  $('#videoImg').attr("src", info.thumbnail);
+  $('#videoTitle').text(info.title);
+  $('#videoDetails').text(info.description);
+}
