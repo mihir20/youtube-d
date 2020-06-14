b253b92ba2a5567f6c42e5aca8dc44f3522a6fc4
mihir20 <mihir20121997@gmail.com>
2020-06-14 15:41:26 +0530

    getting video info

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const youtubedl = require('youtube-dl');
 
 function createWindow() {
   // Create the browser window.
@@ -16,11 +17,24 @@ function createWindow() {
   // receive message from index.html 
   ipcMain.on('asynchronous-message', (event, arg) => {
     console.log(arg);
-
+    downLoadVideo(arg);
     // send message to index.html
     event.sender.send('asynchronous-reply', 'hello');
   });
-  
+}
+
+function downLoadVideo(vidUrl) {
+  const url = 'http://www.youtube.com/watch?v=WKsjaOqDXgg';
+  youtubedl.getInfo(url, (err,info) => {
+    if (err) throw err
+    console.log('id:', info.id)
+    console.log('title:', info.title)
+    console.log('url:', info.url)
+    console.log('thumbnail:', info.thumbnail)
+    console.log('description:', info.description)
+    console.log('filename:', info._filename)
+    console.log('format id:', info.format_id)
+  })
 }
 
 app.whenReady().then(createWindow)
