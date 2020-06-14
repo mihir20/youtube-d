24a6b77948cdc3a7052177ea1300f506f980800d
mihir20 <mihir20121997@gmail.com>
2020-06-14 19:39:54 +0530

    download list

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const youtubedl = require('youtube-dl');
 const fs = require('fs');
 
-let downloadPath = __dirname+'/downloads';
+let downloadPath = __dirname + '/downloads';
 
 function createWindow() {
   // Create the browser window.
@@ -20,37 +20,39 @@ function createWindow() {
   // receive message from index.html 
   ipcMain.on('asynchronous-message', (event, arg) => {
     console.log(arg);
-    downLoadVideo(arg,event);
+    downLoadVideo(arg, event);
     // send message to index.html
     // event.sender.send('asynchronous-reply', 'hello');
   });
 }
 
-function downLoadVideo(vidUrl,event) {
-  const url = 'http://www.youtube.com/watch?v=WKsjaOqDXgg';
-  youtubedl.getInfo(url, (err,info) => {
+function downLoadVideo(url, event) {
+  // const url = 'http://www.youtube.com/watch?v=WKsjaOqDXgg';
+  // const url = 'https://www.youtube.com/playlist?list=PLEFA9E9D96CB7F807'
+  youtubedl.getInfo(url, (err, info) => {
     if (err) throw err
-    console.log('id:', info.id)
-    console.log('title:', info.title)
-    console.log('url:', info.url)
-    console.log('thumbnail:', info.thumbnail)
-    console.log('description:', info.description)
-    console.log('filename:', info._filename)
-    console.log('format id:', info.format_id)
-    event.sender.send('asynchronous-reply', {info});
-    downloadWithFileName(url,info,event);
+    event.sender.send('asynchronous-reply', { info });
+    if(info.length>0){
+      for(var i=0;i<info.length;i++){
+        downloadWithFileName(info[i].webpage_url, info[i], event,i);
+      }
+    }else{
+      downloadWithFileName(url, info, event,null);
+    }
   })
 }
 
-function downloadWithFileName(url,info,event){
-  const video = youtubedl(url,['--format=18']);
+function downloadWithFileName(url, info, event,index) {
+  const video = youtubedl(url, ['--format=18']);
   let pos = 0;
-  video.on('data',function data(chunk){
-    pos+=chunk.length;
-    var progress =info.filesize?(pos / info.filesize * 100).toFixed(2):100;
-    event.sender.send('asynchronous-reply', {progress});
+  video.on('data', function data(chunk) {
+    pos += chunk.length;
+    if (info) {
+      var progress = info.filesize ? (pos / info.filesize * 100).toFixed(2) : 100;
+      event.sender.send('asynchronous-reply', { progress, index });
+    }
   })
-  video.pipe(fs.createWriteStream(downloadPath+'/'+info._filename));
+  video.pipe(fs.createWriteStream(downloadPath + '/' + info._filename));
 }
 
 app.whenReady().then(createWindow)
--- js/index.js
+++ js/index.js
@@ -1,34 +1,69 @@
-$(document).ready(()=>{
+$(document).ready(() => {
+  const { ipcRenderer } = require('electron')
   $('#footer').hide();
-    $('#submitUrlBtn').click(()=>{
-        var url = $('#urlInput').val();
-        
-    const {ipcRenderer} = require('electron')
+  $('#submitUrlBtn').click(() => {
+    var url = $('#urlInput').val();
+    $('#videoList').empty();
 
     // send url to index.js 
-    ipcRenderer.send('asynchronous-message', url )
+    ipcRenderer.send('asynchronous-message', url)
+  })
 
-    // receive message from index.js
-    ipcRenderer.on('asynchronous-reply', (event, arg) => {
-      if(arg.info){
-        setInfo(arg.info);
-      }else if(arg.progress){
-        setProgress(arg.progress);
-      }
+  // receive message from index.js
+  ipcRenderer.on('asynchronous-reply', (event, arg) => {
+    // console.log(arg);
+    if (arg.info) {
+      setInfo(arg.info);
+    } else if (arg.progress) {
+      setProgress(arg.progress, arg.index);
+    }
 
-    })
-
-    })
+  })
 })
 
-const setInfo = (info)=>{
+const setInfo = (info) => {
   $('#footer').show();
-  $('#videoImg').attr("src", info.thumbnail);
-  $('#videoTitle').text(info.title);
-  $('#videoDetails').text(info.description);
+  if (info.length > 0) {
+    for (var i = 0; i < info.length; i++) {
+      var detailsCard =
+        `<div class="card m-3">
+          <div class="media border p-3">
+            <img id="videoImg${i}" alt="John Doe" class="mr-3 mt-3 " src="${info[i].thumbnail}" style="width:100px;">
+            <div class="media-body">
+              <h4 id="videoTitle${i}">${info[i].title}</h4>
+              <p class="truncate" id="videoDetails${i}">${info[i].description}</p>
+              <div class="progress">
+                <div id="progressBar${i}" class="progress-bar bg-success progress-bar-striped progress-bar-animated" style="width:0%"></div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+      $('#videoList').append(detailsCard);
+    }
+  } else {
+    var detailsCard =
+      `<div class="card m-3">
+      <div class="media border p-3">
+        <img id="videoImg" alt="John Doe" class="mr-3 mt-3 " src="${info.thumbnail}" style="width:100px;">
+        <div class="media-body">
+          <h4 id="videoTitle">${info.title}</h4>
+          <p class="truncate" id="videoDetails">${info.description}</p>
+          <div class="progress">
+            <div id="progressBar" class="progress-bar bg-success progress-bar-striped progress-bar-animated" style="width:0%"></div>
+          </div>
+        </div>
+      </div>
+    </div>`
+    $('#videoList').append(detailsCard);
+  }
 }
 
-const setProgress = (progress)=>{
-  $('#progressBar').css("width",`${progress}%`)
-  $('#progressBar').text(`${progress}%`)
+const setProgress = (progress, i) => {
+  if (i != null) {
+    $(`#progressBar${i}`).css("width", `${progress}%`)
+    $(`#progressBar${i}`).text(`${progress}%`)
+  } else {
+    $(`#progressBar`).css("width", `${progress}%`)
+    $(`#progressBar`).text(`${progress}%`)
+  }
 }
